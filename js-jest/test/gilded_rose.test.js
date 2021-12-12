368abc6e85cdfcaf63ff7fe987aecda9a2b18c6d
CjChoiNZ <cjoo.dev@gmail.com>
2021-12-13 01:52:46 +1300

    add all other items test

--- js-jest/test/gilded_rose.test.js
+++ js-jest/test/gilded_rose.test.js
@@ -19,6 +19,90 @@ describe("Gilded Rose", function() {
     expect(items[0].quality).toBe(17);
   });
 
+  it("Aged Brie", function() {
+    const gildedRose = new Shop([new Item("Aged Brie", 2, 0)]);
+    const days = 4;
+    let items;
+    for (let day = 0; day < days; day++) {
+      items = gildedRose.updateQuality();
+    }
+    expect(items[0].name).toBe("Aged Brie");
+    expect(items[0].sellIn).toBe(-2);
+    expect(items[0].quality).toBe(6);
+  });
+
+  it("Elixir of the Mongoose test", function() {
+    const gildedRose = new Shop([new Item("Elixir of the Mongoose", 5, 7)]);
+    const days = 4;
+    let items;
+    for (let day = 0; day < days; day++) {
+      items = gildedRose.updateQuality();
+    }
+    expect(items[0].name).toBe("Elixir of the Mongoose");
+    expect(items[0].sellIn).toBe(1);
+    expect(items[0].quality).toBe(3);
+  });
+
+  it("Sulfuras, Hand of Ragnaros test", function() {
+    const gildedRose = new Shop([new Item("Sulfuras, Hand of Ragnaros", 0, 80)]);
+    const days = 4;
+    let items;
+    for (let day = 0; day < days; day++) {
+      items = gildedRose.updateQuality();
+    }
+    expect(items[0].name).toBe("Sulfuras, Hand of Ragnaros");
+    expect(items[0].sellIn).toBe(0);
+    expect(items[0].quality).toBe(80);
+  });
+
+  it("Sulfuras, Hand of Ragnaros test 2", function() {
+    const gildedRose = new Shop([new Item("Sulfuras, Hand of Ragnaros", -1, 80)]);
+    const days = 4;
+    let items;
+    for (let day = 0; day < days; day++) {
+      items = gildedRose.updateQuality();
+    }
+    expect(items[0].name).toBe("Sulfuras, Hand of Ragnaros");
+    expect(items[0].sellIn).toBe(-1);
+    expect(items[0].quality).toBe(80);
+  });
+
+  it("Backstage passes to a TAFKAL80ETC concert test", function() {
+    const gildedRose = new Shop([new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20)]);
+    const days = 4;
+    let items;
+    for (let day = 0; day < days; day++) {
+      items = gildedRose.updateQuality();
+    }
+    expect(items[0].name).toBe("Backstage passes to a TAFKAL80ETC concert");
+    expect(items[0].sellIn).toBe(11);
+    expect(items[0].quality).toBe(24);
+  });
+
+  it("Backstage passes to a TAFKAL80ETC concert test 2", function() {
+    const gildedRose = new Shop([new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49)]);
+    const days = 4;
+    let items;
+    for (let day = 0; day < days; day++) {
+      items = gildedRose.updateQuality();
+    }
+    expect(items[0].name).toBe("Backstage passes to a TAFKAL80ETC concert");
+    expect(items[0].sellIn).toBe(6);
+    expect(items[0].quality).toBe(50);
+  });
+
+  it("Backstage passes to a TAFKAL80ETC concert test 3", function() {
+    const gildedRose = new Shop([new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49)]);
+    const days = 4;
+    let items;
+    for (let day = 0; day < days; day++) {
+      items = gildedRose.updateQuality();
+    }
+    expect(items[0].name).toBe("Backstage passes to a TAFKAL80ETC concert");
+    expect(items[0].sellIn).toBe(1);
+    expect(items[0].quality).toBe(50);
+  });
+
   it("conjured with normal condition", function() {
     const gildedRose = new Shop([new Item("Conjured", 3, 5)]);
     const items = gildedRose.updateQuality();
