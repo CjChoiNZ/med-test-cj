bf774a3a2d59acdebd5e67b2ad151ca89bb9a1ed
CjChoiNZ <cjoo.dev@gmail.com>
2021-12-13 01:37:48 +1300

    add vest test

--- js-jest/test/gilded_rose.test.js
+++ js-jest/test/gilded_rose.test.js
@@ -7,6 +7,18 @@ describe("Gilded Rose", function() {
     expect(items[0].name).toBe("foo");
   });
 
+  it("+5 Dexterity Vest test", function() {
+    const gildedRose = new Shop([new Item("vest", 1, 20)]);
+    const days = 2;
+    let items;
+    for (let day = 0; day < days; day++) {
+      items = gildedRose.updateQuality();
+    }
+    expect(items[0].name).toBe("vest");
+    expect(items[0].sellIn).toBe(-1);
+    expect(items[0].quality).toBe(17);
+  });
+
   it("conjured with normal condition", function() {
     const gildedRose = new Shop([new Item("Conjured", 3, 5)]);
     const items = gildedRose.updateQuality();
