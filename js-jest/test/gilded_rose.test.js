1bb8d8819d50db6a2fd03db6e1ffe3f2af69a660
CjChoiNZ <cjoo.dev@gmail.com>
2021-12-13 01:17:20 +1300

    add test for conjured with normal condition

--- js-jest/test/gilded_rose.test.js
+++ js-jest/test/gilded_rose.test.js
@@ -6,4 +6,11 @@ describe("Gilded Rose", function() {
     const items = gildedRose.updateQuality();
     expect(items[0].name).toBe("foo");
   });
+
+  it("conjured with normal condition", function() {
+    const gildedRose = new Shop([new Item("Conjured", 3, 5)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].sellIn).toBe(2);
+    expect(items[0].quality).toBe(3);
+  });
 });
